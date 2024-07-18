e07cd11813d2bafadbf5ff1fc496695ea95af036
bunnyrajender55 <149301147+bunnyrajender55@users.noreply.github.com>
2024-07-18 20:07:09 +0530

    Create Task.java

---
+++ src/model/Task.java
@@ -0,0 +1,34 @@
+package model;
+
+import java.io.Serializable;
+
+public class Task implements Serializable {
+    private String description;
+    private boolean isCompleted;
+
+    public Task(String description) {
+        this.description = description;
+        this.isCompleted = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return isCompleted;
+    }
+
+    public void setCompleted(boolean isCompleted) {
+        this.isCompleted = isCompleted;
+    }
+
+    @Override
+    public String toString() {
+        return description + (isCompleted ? " [Completed]" : " [Pending]");
+    }
+}
